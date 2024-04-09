60fdeb07334c21e19620d09e251fa88c560c7f89
MuRong <hi@mrong.me>
2024-04-09 19:27:52 +0800

    feat: red envelope

---
+++ contracts/RedEnvelope.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+contract RedEnvelope {
+    struct Envelope {
+        address sender;
+        address[] receivers;
+        uint256 amount;
+        uint256 balance;
+    }
+
+    uint256 public currentEnvelopeId = 0;
+    mapping(uint256 => Envelope) public envelopes;
+
+    event Receive(address sender, address receiver, uint256 amount);
+    event Create(
+        uint256 envelopeId,
+        address sender,
+        address[] receivers,
+        uint256 amount
+    );
+
+    function create(
+        address[] memory receivers
+    ) public payable checkBalance(msg.value, msg.sender) returns (uint256) {
+        currentEnvelopeId++;
+        envelopes[currentEnvelopeId] = Envelope(
+            msg.sender,
+            receivers,
+            msg.value,
+            msg.value
+        );
+        emit Create(currentEnvelopeId, msg.sender, receivers, msg.value);
+        return currentEnvelopeId;
+    }
+
+    function grab(
+        uint256 id
+    )
+        public
+        checkEnvelopeIsExist(id)
+        checkEnvelopeReceivers(id, msg.sender)
+        returns (uint256)
+    {
+        Envelope memory envelope = envelopes[id];
+        require(envelope.balance > 0, "No balance");
+        uint256 len = envelope.receivers.length;
+        uint256 amount = envelope.amount / len;
+        require(amount <= envelope.balance, "No balance");
+        envelopes[id].balance = envelope.balance - amount;
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Failed to send Ether");
+        emit Receive(envelope.sender, envelope.receivers[0], amount);
+        return amount;
+    }
+
+    function getEnvelope(uint256 id) public view returns (Envelope memory) {
+        return envelopes[id];
+    }
+
+    modifier checkBalance(uint256 amount, address sender) {
+        require(amount <= sender.balance, "Not enough balance");
+        _;
+    }
+
+    modifier checkEnvelopeIsExist(uint256 id) {
+        require(envelopes[id].sender != address(0), "Envelope is not exist");
+        _;
+    }
+
+    modifier checkEnvelopeReceivers(uint256 id, address receiver) {
+        Envelope memory envelope = envelopes[id];
+        bool has = false;
+        for (uint256 i = 0; i < envelope.receivers.length; i++) {
+            if (envelope.receivers[i] == receiver) {
+                has = true;
+                break;
+            } else {
+                has = false;
+            }
+        }
+        require(has, "Receiver is not exist");
+        _;
+    }
+}
---
+++ test/RedEnvelope.js
@@ -0,0 +1,45 @@
+const { ethers } = require("hardhat")
+const { assert, expect } = require("chai")
+
+const abi = require('../artifacts/contracts/RedEnvelope.sol/RedEnvelope.json').abi
+
+describe("RedEnvelope", function () {
+  let redEnvelopeFactory, redEnvelope, signers, contractWithSigner, receivers, owner
+  beforeEach(async function () {
+    redEnvelopeFactory = await ethers.getContractFactory("RedEnvelope")
+    redEnvelope = await redEnvelopeFactory.deploy()
+    signers = await ethers.getSigners();
+    receivers = signers.map(signer => signer.address)
+    owner = signers[0];
+    contractWithSigner = redEnvelope.connect(owner);
+  })
+
+  it("create & grab", async function () {
+    const value = ethers.parseEther("0.1")
+    const tx = await contractWithSigner.create(receivers, { value });
+    await tx.wait();
+    // parse data
+    const currentEnvelopeId = await redEnvelope.currentEnvelopeId();
+    const envelope = await redEnvelope.getEnvelope(currentEnvelopeId)
+    // console.log(envelope)
+    const ownerAddress = await envelope[0]
+    const addresses = envelope[1]
+    const amount = envelope[2]
+    assert.equal(ownerAddress, owner.address);
+    assert.deepEqual(addresses, receivers);
+    assert.equal(amount, value);
+
+    // console.log(currentEnvelopeId)
+    const receiver = signers[1]
+    console.log('receiver old', await receiver.provider.getBalance(receiver.address))
+    const contractWithReceiver = redEnvelope.connect(receiver);
+    const grabTx = await contractWithReceiver.grab(currentEnvelopeId);
+    await grabTx.wait()
+    console.log('amount', ethers.getBigInt(value) / BigInt(receivers.length))
+    console.log('receiver new', await receiver.provider.getBalance(receiver.address))
+
+    // console.log(grabTx)
+    // const balance = await receiver.provider.getBalance(receiver.address);
+    // console.log(balance)
+  })
+})
