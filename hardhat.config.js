e34bd716c428a80637e457f283b9b950f51c9d3b
MuRong <hi@mrong.me>
2024-04-10 13:48:26 +0800

    chore: update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+const { config } = require('dotenv')
+config({ path: `.env` });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: "https://rpc.ankr.com/eth_sepolia",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111
+    }
+  }
 };
